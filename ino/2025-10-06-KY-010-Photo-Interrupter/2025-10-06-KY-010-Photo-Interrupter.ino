d223d8178b8c6ea549a479f217a1659d00ea8b24
John Elliot V <jj5@jj5.net>
2025-11-10 02:44:42 +1100

    Work, work...

---
+++ ino/2025-10-06-KY-010-Photo-Interrupter/2025-10-06-KY-010-Photo-Interrupter.ino
@@ -0,0 +1,25 @@
+
+// 2025-11-10 jj5 - SEE: https://arduinomodules.info/ky-010-photo-interrupter-module/
+
+int Led = 13; // define LED pin
+int buttonpin = 3; // define photo interrupter signal pin
+int val; //define a numeric variable
+
+void setup()
+{
+	pinMode(Led, OUTPUT); // LED pin as output
+	pinMode(buttonpin, INPUT); //photo interrupter pin as input
+}
+
+void loop()
+{
+	val=digitalRead(buttonpin); //read the value of the sensor 
+	if(val == HIGH) // turn on LED when sensor is blocked 
+	{
+		digitalWrite(Led,HIGH);
+	}
+	else
+	{
+		digitalWrite(Led,LOW);
+	}
+}
